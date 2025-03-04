7928fe442357196a059f4833e5f511c929f8b597
Imperor <imperor0103@gmail.com>
2025-03-05 00:33:20 +0900

    [Feat] 무기 장착 애니메이션

--- Assets/Scripts/Item/EquipTool.cs
+++ Assets/Scripts/Item/EquipTool.cs
@@ -17,13 +17,31 @@ public class EquipTool : Equip
     public bool doesDealDamage; // 데미지를 줄 수 있는가?
     public int damage;     // 데미지 양
 
+    // 애니메이션 실행
+    private Animator animator;
+
     private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        
+
+    }
+
+    public override void OnAttackInput()
+    {
+        // isAttacking이 false일때만 내부로직 실행
+        if (!attacking)
+        {
+            attacking = true;
+            animator.SetTrigger("Attack");  // 애니메이션 재생
+            Invoke("OnCanAttack", attackRate);      /// 시간차(공격주기마다 한번 호출)
+        }
+    }
+    void OnCanAttack()
+    {
+        attacking = false;
     }
 }
--- Assets/Scripts/Player/Equipment.cs
+++ Assets/Scripts/Player/Equipment.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // 장착기능
 public class Equipment : MonoBehaviour
 {
-    public Equip curEquip;      // 장착하고 있는 정보
+    public Equip curEquip;      // 현재 장착하고 있는 아이템의 정보
     public Transform equipParent;   // 장비를 달아줄 위치: EquipCamera
 
     // 
@@ -32,6 +33,17 @@ public class Equipment : MonoBehaviour
         {
             Destroy(curEquip.gameObject);
             curEquip = null;
+        }
+    }
+    // 애니메이션 재생
+    public void OnAttackInput(InputAction.CallbackContext context)
+    {
+        // InputActionPhase.Performed: 눌려져 있는 동안
+        // curEquip != null: 현재 장착중인 무기가 있다
+        // controller.canLock: 인벤토리창이 비활성화된 상태
+        if (context.phase == InputActionPhase.Performed && curEquip != null && controller.canLock)
+        {
+            curEquip.OnAttackInput();
         }
     }
 }
